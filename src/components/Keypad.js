947354425181b0b71119c361ae2835912e16990e
Lori Henderson <lorim95@gmail.com>
2022-06-09 16:19:17 -0400

    commit tree 6991b2173b75aa793ee1bb974e1c115a79b157bc

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+
+function Keypad() {
+    function handleKeyPad(e) {
+        console.log("Entering password...")
+    }
+    return (
+        <input type="password" onChange={handleKeyPad}></input>
+        
+    )
+}
+
+export default Keypad
